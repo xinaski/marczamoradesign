7944deef21fdc916f0ea374cb80545208a453c27
xinaski <xinasking@gmail.com>
2020-11-20 02:13:33 +0100

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,22 @@
+const images = document.querySelectorAll('.img');
+const containerImage = document.querySelector('.container-img');
+const imageContainer = document.querySelector('.img-show');
+const copy = document.querySelector('.copy');
+
+images.forEach(image =>{
+  image.addEventListener('click', ()=>{
+    addImage(image.getAttribute('src'), image.getAttribute('alt'));
+  })
+});
+
+const addImage = (srcImage, altImage)=>{
+  containerImage.classList.toggle('move');
+  imageContainer.classList.toggle('show');
+  imageContainer.src = srcImage;
+  copy.innerHTML = altImage;
+}
+
+containerImage.addEventListener('click', ()=>{
+  containerImage.classList.toggle('move');
+  imageContainer.classList.toggle('show');
+})
